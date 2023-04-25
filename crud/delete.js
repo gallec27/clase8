bf1e70b762ed01e646e6f5620bd22aa8576c5482
gallec27 <clement.esteban@gmail.com>
2023-04-25 14:53:00 -0300

    Finalizado

--- crud/delete.js
+++ crud/delete.js
@@ -1,13 +1,12 @@
 const { read } = require("./read");
 const { write } = require("./writeJson");
 
-function del(titulo){
-    const data = read();
-    if (data.some((tarea) => tarea.titulo === titulo)){
-        write(data.filter(tarea => tarea.titulo !== titulo))
-        return console.log("Borrado con éxito.");    
-        //fs.writeFileSync("./tasksData.json", JSON.stringify(.filter(tarea => tarea.titulo !== tituloBorrar)));     
-    }
+function del(titulo) {
+  const data = read();
+  if (data.some((tarea) => tarea.titulo === titulo)) {
+    write(data.filter((tarea) => tarea.titulo !== titulo));
+    return console.log("Borrado con éxito.");
+  }
 }
 
-module.exports = { del }
+module.exports = { del };
